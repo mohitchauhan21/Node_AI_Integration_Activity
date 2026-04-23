b330e3b8153c9102e848aa3d6a0ff81e98ae848a
mohitchauhan21 <mohitchauhan8237@gmail.com>
2026-04-23 14:46:54 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,53 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient, ObjectId } from "mongodb";
+
+const app = express();   // ✅ FIRST create app
+
+app.use(cors());         // ✅ THEN use middleware
+app.use(express.json());
+
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+const dbName = "studentDB";
+
+let students;
+
+async function connectDB() {
+  await client.connect();
+  console.log("Connected to MongoDB");
+  const db = client.db(dbName);
+  students = db.collection("students");
+}
+connectDB();
+
+// ROUTES
+
+app.post("/students", async (req, res) => {
+  const result = await students.insertOne(req.body);
+  res.send(result);
+});
+
+app.get("/students", async (req, res) => {
+  const data = await students.find().toArray();
+  res.send(data);
+});
+
+app.put("/students/:id", async (req, res) => {
+  const result = await students.updateOne(
+    { _id: new ObjectId(req.params.id) },
+    { $set: req.body }
+  );
+  res.send(result);
+});
+
+app.delete("/students/:id", async (req, res) => {
+  const result = await students.deleteOne({
+    _id: new ObjectId(req.params.id),
+  });
+  res.send(result);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
